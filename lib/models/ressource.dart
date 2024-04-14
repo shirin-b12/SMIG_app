b1129d9f29ee8efdf414b84ac5088c34f1210c52
Maxence Guenin <maxence@dataraxys.com>
2024-04-14 21:50:13 +0200

    modification ressource et ajour de la ffichage de categorie et de type

--- lib/models/ressource.dart
+++ lib/models/ressource.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:smig_app/models/categorie.dart';
+import 'package:smig_app/models/tag.dart';
+import 'package:smig_app/models/type.dart';
 import 'package:smig_app/models/utilisateur.dart';
 
 class Ressource {
-
   final int id;
   final String titre;
   final String description;
@@ -12,39 +14,47 @@ class Ressource {
   final String date_de_creation;
   final int visibilite;
   final Utilisateur createur;
+  final Categorie category;
+  final Type type;
 
   Ressource(
-      {
-        required this.id,
-        required this.titre,
-        required this.description,
-        required this.image,
-        required this.vue,
-        required this.date_de_creation,
-        required this.visibilite,
-        required this.createur
-      });
+      {required this.id,
+      required this.titre,
+      required this.description,
+      required this.image,
+      required this.vue,
+      required this.date_de_creation,
+      required this.visibilite,
+      required this.createur,
+      required this.category,
+      required this.type});
 
   factory Ressource.fromJson(Map<String, dynamic> json) {
     DateTime dateTime = DateTime.parse(json['date_de_creation']);
     String formattedDate = DateFormat("dd/MM/yyyy HH:mm:ss").format(dateTime);
 
     return Ressource(
-      id: json['id_ressource'],
-      createur: Utilisateur.fromJson(json['createur'] as Map<String, dynamic>),
-      titre: json['titre'],
-      description: json['description'],
-      image: json['image'],
-      vue: json['vue'],
-      date_de_creation: formattedDate,
-      visibilite: json['visibilite'],
-    );
+        id: json['id_ressource'],
+        createur:
+            Utilisateur.fromJson(json['createur'] as Map<String, dynamic>),
+        titre: json['titre'],
+        description: json['description'],
+        image: json['image'],
+        vue: json['vue'],
+        date_de_creation: formattedDate,
+        visibilite: json['visibilite'],
+        category: json['categorie'] != null
+            ? Categorie.fromJson(json['categorie'] as Map<String, dynamic>)
+            : Categorie(id: 0, nom: "Aucune catégorie"),
+        type: json['type'] != null
+            ? Type.fromJson(json['type'] as Map<String, dynamic>)
+            : Type(id: 0, nom: "Aucun type"));
   }
 
-  String getDateWithoutSeconds(){
+  String getDateWithoutSeconds() {
     String i = "";
 
-    for(int p = 0; p <= (date_de_creation.length - 4); p++){
+    for (int p = 0; p <= (date_de_creation.length - 4); p++) {
       i += date_de_creation[p];
     }
 
--- lib/widgets/ressource_card.dart
+++ lib/widgets/ressource_card.dart
@@ -25,81 +25,106 @@ class RessourceCard extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              FutureBuilder(
-                future: this.ressource.createur.pic != null
-                    ? api.fetchImage(this.ressource.createur.pic as String)
-                    : null,
-                builder:
-                    (BuildContext context, AsyncSnapshot<Image?> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Icon(Icons.error);
-                  } else {
-                    return CircleAvatar(
-                      backgroundColor: Colors.grey[200],
-                      backgroundImage: snapshot.data != null
-                          ? snapshot.data!.image as ImageProvider
-                          : null,
-                      child: snapshot.data == null
-                          ? const Icon(Icons.image, color: Color(0xFF03989E))
-                          : null,
-                    );
-                  }
-                },
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Text(
-                  '${ressource.createur.nom} ${ressource.createur.prenom}',
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF015E62),
+      child: Padding(
+        padding: const EdgeInsets.all(38.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FutureBuilder(
+                  future: this.ressource.createur.pic != null
+                      ? api.fetchImage(this.ressource.createur.pic as String)
+                      : null,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<Image?> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Icon(Icons.error);
+                    } else {
+                      return CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        backgroundImage: snapshot.data != null
+                            ? snapshot.data!.image as ImageProvider
+                            : null,
+                        child: snapshot.data == null
+                            ? const Icon(Icons.image, color: Color(0xFF03989E))
+                            : null,
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    '${ressource.createur.nom} ${ressource.createur.prenom}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF015E62),
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Text(
+                ressource.titre,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF015E62),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Center(
-            child: Text(
-              ressource.titre,
+            ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Catégorie: ${ressource.category.nom}",
+                  style: const TextStyle(
+                    color: Color(0xFF015E62),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "Type: ${ressource.type.nom}",
+                  style: const TextStyle(
+                    color: Color(0xFF015E62),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Container(
+                  height: 70.0,
+                  width: 70.0,
+                  color: Color(0xFF03989E),
+                  child: ressource.image == null
+                      ? const Icon(Icons.image, color: Color(0xFFFFFFFF))
+                      : null,
+                ),
+              ),
+            ),
+            Text(
+              ressource.description,
               style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF015E62),
+                color: Colors.black54,
               ),
+              overflow: TextOverflow.ellipsis,
             ),
-          ),
-          const SizedBox(height: 5),
-          Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Container(
-                height: 70.0,
-                width: 70.0,
-                color: Color(0xFF03989E),
-                child: ressource.image == null
-                    ? const Icon(Icons.image, color: Color(0xFFFFFFFF))
-                    : null,
-              ),
-            ),
-          ),
-          Text(
-            ressource.description,
-            style: const TextStyle(
-              color: Colors.black54,
-            ),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
